a6e0e341a8def72f56e07b279f4354064e3a9b73
oliver.bruentje <oliver.bruentje@openknowledge.de>
2021-10-06 13:39:43 +0200

    Change categorial columns to pandas Int64 dtype

--- preprocessor.py
+++ preprocessor.py
@@ -1,6 +1,8 @@
-import string
+from typing import Sequence
 
+import re
 import numpy as np
+import pandas as pd
 
 from pandas import DataFrame, Series, concat
 from pandas.core.groupby import SeriesGroupBy, DataFrameGroupBy
@@ -8,15 +10,25 @@ from sklearn.base import TransformerMixin
 from sklearn.impute import SimpleImputer
 from sklearn.neighbors import LocalOutlierFactor
 from sklearn.preprocessing import OrdinalEncoder, OneHotEncoder
+from sklearn.utils._testing import MinimalTransformer
 
 
-class Preprocessor(TransformerMixin):
+class Preprocessor(TransformerMixin, MinimalTransformer):
 
-    def __init__(self, drop_original_columns: bool = True) -> None:
+    def __init__(self, drop_original_columns: bool = True, engineer_title: bool = True, drop_nans: bool=True, engineer_deck: bool = True) -> None:
         super().__init__()
+        self.engineer_deck_ = engineer_deck
+        self.drop_nans = drop_nans
+        self.engineer_title_ = engineer_title
         self.drop_original_columns = drop_original_columns
+        self.title_list_ = ['Mrs', 'Mr', 'Master', 'Miss', 'Major', 'Rev',
+                                      'Dr', 'Ms', 'Mlle','Col', 'Capt', 'Mme', 'Countess',
+                                      'Don', 'Jonkheer']
 
-    def fit_transform(self, X, y=None, **fit_params):
+    def fit(self, X, y=None, **fit_params) -> object:
+        return self
+
+    def transform(self, X, y=None):
         """
         Preprocessing: Sex, Embarked -> Categorial
         Preprocessing: Drop Columns Name, Ticket, Cabin
@@ -28,28 +40,31 @@ class Preprocessor(TransformerMixin):
         # todo: fare & age -> set median on nan
 
         # feature engineering
-        df_pp = self.engineer_deck(X)
-        if self.drop_original_columns:
-            df_pp = df_pp.drop('Cabin', axis=1)
-
-        # engineer Deck_cat
-        df_pp = self.ordinal_categories(df_pp, ['Deck'])
-        if self.drop_original_columns:
-            df_pp = df_pp.drop('Deck', axis=1)
-
+        # DECK
+        df_pp = X
+        if self.engineer_deck_:
+            df_pp = self.engineer_deck(X, 'Deck')
+            # engineer Deck_cat
+            df_pp = self.ordinal_categories(df_pp, ['Deck'])
+            if self.drop_original_columns:
+                df_pp = df_pp.drop('Deck', axis=1)
             # deck missing indicator
-        df_pp = self.engineer_deck_missing(df_pp, column_name='Deck_cat_missing')
+            df_pp = self.engineer_deck_missing_indicator(df=df_pp, column_name='Deck_cat_missing')
+            # calculate median Deck no for each class to fill nan of column Deck
+            df_pp = self.fill_missing_deck(df_pp)
 
-        # calculate median Deck no for each class to fill nan of column Deck
-        df_pp = self.fill_missing_deck(df_pp)
 
+        # AGE: fill nan of age with median age
         df_pp = self.fill_age(df_pp)
 
-        df_pp = df_pp.dropna()
+        # TITLE
+        if self.engineer_title_:
+            df_pp = self.engineer_title(df_pp, 'Title')
+            df_pp = self.ordinal_categories(df_pp, ['Title'])
+            if self.drop_original_columns:
+                df_pp = df_pp.drop(['Title'], axis=1)
 
         df_pp = self.ordinal_categories(df_pp, ['Sex', 'Embarked'])
-        if self.drop_original_columns:
-            df_pp = df_pp.drop(['Sex', 'Embarked'], axis=1)
 
 
         # for col in ["Sex_cat", "Parch", "Pclass", "SibSp", "Embarked_cat"]:
@@ -57,9 +72,11 @@ class Preprocessor(TransformerMixin):
         #     if self.drop_original_columns:
         #         df_pp = df_pp.drop(col, axis=1)
 
+        if self.drop_original_columns:
+            df_pp = df_pp.drop(axis=1, labels=["PassengerId", "Name", "Ticket", 'Cabin', 'Sex', 'Embarked'])
 
-
-        df_pp = self.drop_unrelevant_columns(df_pp)
+        if self.drop_nans:
+            df_pp = df_pp.dropna()
 
         # df_pp = self.outlier_detection(df_pp)
 
@@ -68,11 +85,11 @@ class Preprocessor(TransformerMixin):
     def fill_missing_deck(self, df: DataFrame) -> DataFrame:
 
         pclass_group: DataFrameGroupBy = df.groupby(by='Pclass')
-        pclass_to_deck_median: SeriesGroupBy = df.groupby(by='Pclass').Deck_cat.mean()
+        pclass_to_deck_median: Series = df.groupby(by='Pclass').Deck_cat.mean().astype(int)
 
         def handle_pclass_group(group: DataFrame):
-            def fill_deck(row: Series):
-                if np.isnan(row['Deck_cat']):
+            def fill_deck(row: Series) -> Series:
+                if pd.isna(row['Deck_cat']):
                     row_tmp = row.copy()
                     row_tmp['Deck_cat'] = pclass_to_deck_median[row['Pclass']]
                     return row_tmp
@@ -87,33 +104,24 @@ class Preprocessor(TransformerMixin):
         return df_transformed.astype(dtype={'Deck_cat': 'int'})
 
 
-    def ordinal_categories(self, df: DataFrame, cols: [string]) -> DataFrame:
+    def ordinal_categories(self, data: DataFrame, cols: [str]) -> DataFrame:
         """
         Preprocessing: Transform object columns to categories and appends '_cat' to column name
         :param df:
         :return:
         """
-        encoder = OrdinalEncoder(dtype=int)
-        df_tmp = df.dropna()
-        np_encoded_cat = encoder.fit_transform(df_tmp[cols])
-        df_cat = DataFrame(data=np_encoded_cat, columns=["{}_cat".format(col) for col in cols], index=df_tmp.index)
-        return concat([df, df_cat], axis=1)
+        encoder = OrdinalEncoder(dtype=float)
+        np_encoded_cat = encoder.fit_transform(data[cols])
+        # DataFrame with dtype  pd.Int64Dtype() for having ints with pd.NA (works cause dropna is called later so no ps.NA values
+        df_cat = DataFrame(data=np_encoded_cat, columns=["{}_cat".format(col) for col in cols], index=data.index, dtype=pd.Int64Dtype())
+        return concat([data, df_cat], axis=1)
 
-    def onehot_categories(self, df: DataFrame, column: string) -> DataFrame:
+    def onehot_categories(self, df: DataFrame, column: str) -> DataFrame:
         encoder = OneHotEncoder(sparse=False)
         np_encoded: np.array = encoder.fit_transform(df[[column]])
         feature_names: np.array = encoder.get_feature_names_out()
         df_encoded = DataFrame(data=np_encoded, columns=feature_names.tolist(), dtype=bool, index=df.index)
         return concat([df, df_encoded], axis=1)
-
-    def drop_unrelevant_columns(self, df: DataFrame) -> DataFrame:
-        """
-         Preprocessing: Drop Columns
-        :param df:
-        :return:
-        """
-        return df\
-            .drop(axis=1, labels=["PassengerId", "Name", "Ticket"])
 
     def outlier_detection(self, df: DataFrame) -> DataFrame:
         lof = LocalOutlierFactor(n_neighbors=20)
@@ -121,15 +129,15 @@ class Preprocessor(TransformerMixin):
 
         return df.iloc[lof_mask != -1]
 
-    def engineer_deck(self, df_pp: DataFrame) -> DataFrame:
+    def engineer_deck(self, df_pp: DataFrame, column_name: str) -> DataFrame:
 
         s_deck: Series = df_pp['Cabin'][df_pp['Cabin'].notna()]\
             .map(lambda c:c.strip())\
             .map(lambda c:c[:1])
 
-        return df_pp.assign(Deck=s_deck)
+        return df_pp.assign(**{'{}'.format(column_name): s_deck})
 
-    def engineer_deck_missing(self, df: DataFrame, column_name: string) -> DataFrame:
+    def engineer_deck_missing_indicator(self, df: DataFrame, column_name: str) -> DataFrame:
         """
         Adds column indicating whether Deck_cat has missing values
         :param df:
@@ -147,7 +155,17 @@ class Preprocessor(TransformerMixin):
 
         return df_tmp
 
+    def engineer_title(self, df: DataFrame, column_name: str) -> DataFrame:
+        this = self
+        def find_first_element_in_titlelist(elements: Sequence[str]) -> str:
+            found_elements: list[str] = [i for i in elements if i in this.title_list_]
+            if len(found_elements) > 0:
+                return found_elements[0]
+            else:
+                return np.nan
 
-
-
-
+        df_tmp = df.copy()
+        df_tmp[column_name] = df['Name']\
+            .map(lambda name:re.split('\s|\,|\.', name))\
+            .map(find_first_element_in_titlelist)
+        return df_tmp
